3742a5adeb092ff55f6639fa1b880fdd116fe028
dfarina92 <49841514+dfarina92@users.noreply.github.com>
2019-04-30 20:02:39 -0300

    Producto
    Producto

--- MaxiKiosko/Clases/Cliente.cs
+++ MaxiKiosko/Clases/Cliente.cs
@@ -9,68 +9,68 @@ using System.Data;
 
 namespace MaxiKiosko
 {
-    class Cliente
+    class Producto
     {
         Conexion _BD = new Conexion();
 
-        public int idCliente { get; set; }
-        public string apellido { get; set; }
-        public string nombre { get; set; }
-        public string telefono { get; set; }
-        public string email { get; set; }
+        public int id_producto { get; set; }
+        public float precio { get; set; }
+        public int stock { get; set; }
+        public string tipo_medida { get; set; }
+        public string descripcion { get; set; }
 
-        public Cliente (string papellido, string pnombre, string ptelefono, string pemail )
+        public Producto(float precio, int stock, string tipo_medida, string descripcion)
         {
-            this.apellido = papellido;
-            this.nombre = pnombre;
-            this.telefono = ptelefono;
-            this.email = pemail;
+            this.precio = precio;
+            this.stock = stock;
+            this.tipo_medida = tipo_medida;
+            this.descripcion = descripcion;
         }
 
 
-        public Cliente()
+        public Producto()
         {
            
         }
 
-        public void agregarCliente(  )
+        public void agregarProducto(  )
         {
-            string SqlInsert = @" INSERT INTO cliente
-                         (apellido, nombre, telefono, email) VALUES ('" +
-                            this.apellido + "', '" +
-                            this.nombre + "', '" +
-                            this.telefono + "', '" +
-                            this.email +
+            string SqlInsert = @" INSERT INTO producto
+                         (precio, stock, tipo_medida, descripcion) VALUES ('" +
+                            this.precio + "', '" +
+                            this.stock + "', '" +
+                            this.tipo_medida + "', '" +
+                            this.descripcion +
                              "')";
             this._BD.grabar_modificar(SqlInsert);
         }
 
-        public void modificarCliente()
+        public void modificarProducto()
         {
-            string SqlUpdate = @" Update cliente
-                               SET nombre = '" + this.nombre + "'," +
-                               "apellido = '" + this.apellido + "'," +
-                               "telefono = '" + this.telefono + "'," +
-                               "email = '" + this.email + "'" +
-                               " WHERE idCliente = " + this.idCliente;
+            string SqlUpdate = @" Update producto
+                               SET precio = '" + this.precio + "'," +
+                               "stock = '" + this.stock + "'," +
+                               "tipo_medida = '" + this.tipo_medida + "'," +
+                               "descripcion = '" + this.descripcion + "'" +
+                               " WHERE id_producto = " + this.id_producto;
             this._BD.grabar_modificar(SqlUpdate);
         } 
 
-        public DataTable consultarCliente(string subString) {
-            return this._BD.consulta(String.Format ("SELECT * FROM cliente WHERE nombre LIKE '%{0}%'" +
-                " OR apellido LIKE '%{0}%'" +
-                " OR telefono LIKE '%{0}%' " +
-                " OR email LIKE '%{0}%'", subString));
+        public DataTable consultarProducto(string subString) {
+            return this._BD.consulta(String.Format ("SELECT * FROM producto WHERE precio LIKE '%{0}%'" +
+                " OR stock LIKE '%{0}%'" +
+                " OR tipo_medida LIKE '%{0}%' " +
+                " OR descripcion LIKE '%{0}%'", subString));
         }
 
         public DataTable buscarTodos()
         {
-            return this._BD.consulta("SELECT * FROM cliente");
+            return this._BD.consulta("SELECT * FROM producto");
         }
 
-        public void borrar(int idCliente)
+        public void borrarProducto(int id_producto)
         {
-            this._BD.grabar_modificar("DELETE FROM cliente WHERE idCliente = " + idCliente);
+            this._BD.grabar_modificar("DELETE FROM producto WHERE id_producto = " + id_producto);
         }
     }
 }
---
+++ MaxiKiosko/Clases/Producto.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+using System.Data;
+
+namespace MaxiKiosko
+{
+    class Cliente
+    {
+        Conexion _BD = new Conexion();
+
+        public int idCliente { get; set; }
+        public string apellido { get; set; }
+        public string nombre { get; set; }
+        public string telefono { get; set; }
+        public string email { get; set; }
+
+        public Cliente (string papellido, string pnombre, string ptelefono, string pemail )
+        {
+            this.apellido = papellido;
+            this.nombre = pnombre;
+            this.telefono = ptelefono;
+            this.email = pemail;
+        }
+
+
+        public Cliente()
+        {
+           
+        }
+
+        public void agregarCliente(  )
+        {
+            string SqlInsert = @" INSERT INTO cliente
+                         (apellido, nombre, telefono, email) VALUES ('" +
+                            this.apellido + "', '" +
+                            this.nombre + "', '" +
+                            this.telefono + "', '" +
+                            this.email +
+                             "')";
+            this._BD.grabar_modificar(SqlInsert);
+        }
+
+        public void modificarCliente()
+        {
+            string SqlUpdate = @" Update cliente
+                               SET nombre = '" + this.nombre + "'," +
+                               "apellido = '" + this.apellido + "'," +
+                               "telefono = '" + this.telefono + "'," +
+                               "email = '" + this.email + "'" +
+                               " WHERE idCliente = " + this.idCliente;
+            this._BD.grabar_modificar(SqlUpdate);
+        } 
+
+        public DataTable consultarCliente(string subString) {
+            return this._BD.consulta(String.Format ("SELECT * FROM cliente WHERE nombre LIKE '%{0}%'" +
+                " OR apellido LIKE '%{0}%'" +
+                " OR telefono LIKE '%{0}%' " +
+                " OR email LIKE '%{0}%'", subString));
+        }
+
+        public DataTable buscarTodos()
+        {
+            return this._BD.consulta("SELECT * FROM cliente");
+        }
+
+        public void borrar(int idCliente)
+        {
+            this._BD.grabar_modificar("DELETE FROM cliente WHERE idCliente = " + idCliente);
+        }
+    }
+}
