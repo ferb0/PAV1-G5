28971e9f8d519b3da92a3e0803dd2b842f7e115f
dfarina92 <49841514+dfarina92@users.noreply.github.com>
2019-05-04 18:16:57 -0300

    Corrigiendo clases culpa de visual de mierda
    Corrigiendo clases culpa de visual de mierda

--- MaxiKiosko/Clases/Conexion.cs
+++ MaxiKiosko/Clases/Conexion.cs
@@ -12,7 +12,7 @@ namespace MaxiKiosko
     class Conexion
     {
         MySqlCommand cmd = new MySqlCommand();
-        MySqlConnection conexion = new MySqlConnection("server=localhost;user id=root;database=maxikiosko;allowuservariables=True; password=admin;");
+        MySqlConnection conexion = new MySqlConnection("server=localhost;user id=root;database=maxiKiosco;allowuservariables=True; password=damian;");
         
 
         public void conectar()
--- MaxiKiosko/Clases/Proveedor.cs
+++ MaxiKiosko/Clases/Proveedor.cs
@@ -15,7 +15,7 @@ namespace MaxiKiosko
 
         public int cuit { get; set; }
         public string razon_social { get; set; }
-        public int telefono { get; set; }
+        public string telefono { get; set; }
         public string email { get; set; }
         
 
